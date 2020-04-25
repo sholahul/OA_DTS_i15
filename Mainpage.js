444a71c81d57c5561048369072677fe98f43b5da
sholahul <sholahulfajri30@gmail.com>
2020-04-25 13:12:47 +0700

    new new

---
+++ Mainpage.js
@@ -0,0 +1,17 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("heading").style.background = "rgb(172,185,202)";
+        document.getElementById("heading").style.paddingTop = "5px";
+
+    } else {
+        document.getElementById("heading").style.background = 'transparent';
+        document.getElementById("heading").style.paddingTop = "20px";
+
+    }
+}
+
